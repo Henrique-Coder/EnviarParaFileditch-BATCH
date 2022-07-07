b0faf1f6bc95d615344ca438a36bc8662772500a
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-07-07 18:00:38 -0300

    Modificações gerais e otimização geral do script
    / Enviar para Fileditch.bat

--- Enviar para Fileditch.bat
+++ Enviar para Fileditch.bat
@@ -2,34 +2,61 @@
 
 @echo off
 
+:: Título
+title Enviar para Fileditch - Por FireHDPower#5551
+
 :: Setando cor do texto no CMD
 color F >nul 2>&1
 
 :: Avisos legais para ler antes do uso do script e criador do código
-echo - ANTES DE UPAR QUALQUER ARQUIVO, LEIA OS TERMOS DO SITE FILEDITCH: https://fileditch.com/faq.html
-echo - ARQUIVOS COM EXTENSOES POTENCIALMENTE PERIGOSAS, COMO .EXE, NAO SERAO ENVIADAS AO SITE
-echo - VIDEOS E IMAGENS SAO PERMITIDAS, PARA UPAR OUTRO TIPO DE ARQUIVO, ZIPE PARA EVITAR ERROS
-echo - O SCRIPT SUPORTA APENAS UM UPLOAD POR VEZ, CASO SELECIONE DOIS OU MAIS, TERA ERROS
 echo.
-echo CLIQUE EM QUALQUER TECLA PARA INICIAR O UPLOAD DO ARQUIVO - Criador: FireHDPower#5551 - %date% - %time%
+echo CREDITOS DO CRIADOR E REPOSITORIO NO GITHUB
+echo.
+echo - Discord do Criador: FireHDPower#5551
+echo - Repositorio no GitHub: https://github.com/Henrique-Coder/EnviarParaFileditch-BATCH
+echo.
+echo -----------------------------------------------------------------------------------------------------------------------
+echo.
+echo AVISOS LEGAIS SOBRE O UPLOAD NO FILEDITCH
+echo.
+echo - Leia atentamente ao FAQ do site: https://fileditch.com/faq.html
+echo - Arquivos .EXE, .JAR e outros executaveis nao sao permitidos
+echo - O script suporta apenas UM UPLOAD por vez, espere terminar antes de iniciar um novo
+echo - Arquivos zipados, videos e imagens sao permitidos (Lembre-se que arquivos .MKV nao sao reproduzidos no navegador)
+echo.
+echo -----------------------------------------------------------------------------------------------------------------------
+echo.
+echo.
+echo - Arquivo: %1
+echo.
+echo.
+echo ------------------------------ CLIQUE EM QUALQUER TECLA PARA INICIAR O UPLOAD DO ARQUIVO ------------------------------
 
 :: Clique em qualquer tecla para continuar o upload do arquivo
 pause >nul 2>&1
 
 :: Limpando a tela do CMD e repassando os avisos legais seguido do upload em tempo real
 cls
-echo - ANTES DE UPAR QUALQUER ARQUIVO, LEIA OS TERMOS DO SITE FILEDITCH: https://fileditch.com/faq.html
-echo - ARQUIVOS COM EXTENSOES POTENCIALMENTE PERIGOSAS, COMO .EXE, NAO SERAO ENVIADAS AO SITE
-echo - VIDEOS E IMAGENS SAO PERMITIDAS, PARA UPAR OUTRO TIPO DE ARQUIVO, ZIPE PARA EVITAR ERROS
-echo - O SCRIPT SUPORTA APENAS UM UPLOAD POR VEZ, CASO SELECIONE DOIS OU MAIS, TERA ERROS
+echo.
+echo - Data de inicio: %date%
+echo - Hora de inicio: %time%
+echo.
+echo -----------------------------------------------------------------------------------------------------------------------
+echo.
+echo CREDITOS DO CRIADOR E REPOSITORIO NO GITHUB
+echo.
+echo - Discord do Criador: FireHDPower#5551
+echo - Repositorio no GitHub: https://github.com/Henrique-Coder/EnviarParaFileditch-BATCH
+echo.
+echo -----------------------------------------------------------------------------------------------------------------------
 echo.
 echo.
 
 :: Upando arquivo para o Fileditch e criando arquivo de texto com o link do arquivo enviado
-curl -i -F files[]=@%1 https://up1.fileditch.com/upload.php | findstr "\"url\"" > %temp%/sendto-fileupload-link.txt
+curl -i -F files[]=@%1 https://up1.fileditch.com/upload.php | findstr "\"url\"" > %temp%/sendto-fileupload-fileditch-link.txt
 
 :: Verificando arquivo de texto e limpando caracteres
-set /p link_upload=< %temp%/sendto-fileupload-link.txt
+set /p link_upload=< %temp%/sendto-fileupload-fileditch-link.txt
 set link_upload=%link_upload:"url":=%
 set link_upload=%link_upload:"=%
 set link_upload=%link_upload:,=%
@@ -37,13 +64,23 @@ set link_upload=%link_upload: =%
 set link_upload=%link_upload:\/=/%
 
 :: Copiando link de upload para a área de transferência
-echo %link_upload% | clip
+echo %link_upload% | clip >nul 2>&1
+
+:: Um arquivo de texto temporário foi criado aqui: %temp%/sendto-fileupload-fileditch-link.txt
+:: Este arquivo serve apenas para copiar o link após o upload do arquivo, o mesmo poderá ser apagado após o upload estar completo
 
 :: Mostrando o link e diretorio do arquivo enviado
 echo.
 echo.
+echo -----------------------------------------------------------------------------------------------------------------------
+echo.
 echo O link de download/embed foi copiado para a area de transferencia
 echo.
-echo - Diretorio: %1
+echo - Arquivo: %1
 echo - Link: %link_upload%
+echo.
+echo -----------------------------------------------------------------------------------------------------------------------
+echo.
+echo - Data de termino: %date%
+echo - Hora de termino: %time%
 pause >nul 2>&1
