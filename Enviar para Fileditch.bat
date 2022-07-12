d6c90d61c4111e70e06a74ce4104a1c58a7d14bb
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-07-12 18:55:26 -0300

    Atualização para melhora do código

--- Enviar para Fileditch.bat
+++ Enviar para Fileditch.bat
@@ -1,4 +1,8 @@
-:: Batch desenvolvido por: FireHDPower#5551
+:: Este arquivo foi desenvolvido pelo: FirePower#5551
+:: This file was developed by: FirePower#5551
+
+:: [PT-BR] Por favor, caso você for usar partes desse script, deixe o meu repositório ou o meu ID do Discord nos créditos do seu arquivo, ou vídeo
+:: [EN-US] Please, if you are going to use parts of this script, leave my repository or my Discord ID in the credits of your file, or video
 
 @echo off
 
@@ -8,9 +12,17 @@ title Enviar para Fileditch - Por FireHDPower#5551
 :: Setando cor do texto no CMD
 color F >nul 2>&1
 
-:: Avisos legais para ler antes do uso do script e criador do código
-echo.
-echo CREDITOS DO CRIADOR E REPOSITORIO NO GITHUB
+:: Tamanhos do arquivo
+set Tamanho=%~z1
+set B=%Tamanho% B
+set /a KB="Tamanho/1000"
+set KB=%KB% KB
+set /a MB="Tamanho/1000000"
+set MB=%MB% MB
+set /a GB="Tamanho/1000000000"
+set GB=%GB% GB
+
+:: Criador do código, avisos legais antes do upload e informações do arquivo
 echo.
 echo - Discord do Criador: FireHDPower#5551
 echo - Repositorio no GitHub: https://github.com/Henrique-Coder/EnviarParaFileditch-BATCH
@@ -26,9 +38,10 @@ echo - Arquivos zipados, videos e imagens sao permitidos (Lembre-se que arquivos
 echo.
 echo -----------------------------------------------------------------------------------------------------------------------
 echo.
-echo.
-echo - Arquivo: %1
-echo.
+echo - Diretorio: %~1
+echo - Nome: %~n1
+echo - Extensao: %~x1
+echo - Tamanho: %GB% / %MB% / %KB% / %B%
 echo.
 echo ------------------------------ CLIQUE EM QUALQUER TECLA PARA INICIAR O UPLOAD DO ARQUIVO ------------------------------
 
@@ -38,18 +51,15 @@ pause >nul 2>&1
 :: Limpando a tela do CMD e repassando os avisos legais seguido do upload em tempo real
 cls
 echo.
-echo - Data de inicio: %date%
-echo - Hora de inicio: %time%
-echo.
-echo -----------------------------------------------------------------------------------------------------------------------
-echo.
-echo CREDITOS DO CRIADOR E REPOSITORIO NO GITHUB
-echo.
 echo - Discord do Criador: FireHDPower#5551
 echo - Repositorio no GitHub: https://github.com/Henrique-Coder/EnviarParaFileditch-BATCH
 echo.
 echo -----------------------------------------------------------------------------------------------------------------------
 echo.
+echo - Data de inicio: %date%
+echo - Hora de inicio: %time%
+echo.
+echo -----------------------------------------------------------------------------------------------------------------------
 echo.
 
 :: Upando arquivo para o Fileditch e criando arquivo de texto com o link do arquivo enviado
@@ -68,13 +78,15 @@ echo %link_upload% | clip >nul 2>&1
 
 :: Mostrando o link e diretorio do arquivo enviado
 echo.
-echo.
 echo -----------------------------------------------------------------------------------------------------------------------
 echo.
 echo O link de download/embed foi copiado para a area de transferencia
 echo.
-echo - Arquivo: %1
 echo - Link: %link_upload%
+echo.
+echo - Diretorio: %~1
+echo - Arquivo: %~n1%~x1
+echo - Tamanho: %GB% / %MB% / %KB% / %B%
 echo.
 echo -----------------------------------------------------------------------------------------------------------------------
 echo.
